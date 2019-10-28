f19870ceda963fb85c5f18464a28296ad9675afd
Michael Stewart <mikrostew@gmail.com>
2019-10-28 11:57:16 -0700

    remove samsung bloatware from my phone
    I've clicked on the wrong apps too many times

---
+++ scripts/samsung-bloatware-remove.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+# Remove bloatware from my Samsung Phone
+
+# References:
+# https://forum.xda-developers.com/galaxy-s9-plus/help/list-bloatware-youd-recommend-to-rid-off-t3759500
+# https://medium.com/@kaikoenig/samsungs-bloatware-disgrace-c7d14a298ad7
+
+# typical output:
+#
+# $ adb shell "pm uninstall -k --user 0 com.samsung.android.app.spage"
+# Success
+#
+# $ adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.agent"
+# Failure [not installed for 0]
+
+
+# can find most of these using `adb shell` then `pm list packages -f <name>`
+
+
+##########################
+# useless or worse
+##########################
+
+# Bixby - this is terrible, no one should ever use it for anything
+adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.agent"
+adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.es.globalaction"
+adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.wakeup"
+adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.plmsync"
+adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.voiceinput"
+adb shell "pm uninstall -k --user 0 com.samsung.systemui.bixby"
+adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.agent.dummy"
+adb shell "pm uninstall -k --user 0 com.samsung.android.bixbyvision.framework"
+adb shell "pm uninstall -k --user 0 com.samsung.android.app.spage"
+
+# Yahoo - why?
+adb shell "pm uninstall -k --user 0 com.samsung.android.widgetapp.yahooedge.finance"
+adb shell "pm uninstall -k --user 0 com.samsung.android.widgetapp.yahooedge.sport"
+
+# Flipboard
+adb shell "pm uninstall -k --user 0 flipboard.boxer.app"
+
+# Skype
+adb shell "pm uninstall -k --user 0 com.skype.raider"
+
+# Kids Installer - what is this?
+adb shell "pm uninstall -k --user 0 com.samsung.android.kidsinstaller"
+
+# Samsung Clousd
+adb shell "pm uninstall -k --user 0 com.samsung.android.scloud"
+adb shell "pm uninstall -k --user 0 com.samsung.android.samsungpass"
+adb shell "pm uninstall -k --user 0 com.samsung.android.samsungpassautofill"
+
+
+##########################
+# duplicate apps
+##########################
+
+# messaging
+adb shell "pm uninstall -k --user 0 com.samsung.android.messaging"
+# email
+adb shell "pm uninstall -k --user 0 com.samsung.android.email.provider"
+# contacts
+adb shell "pm uninstall -k --user 0 com.samsung.android.contacts"
+# clock
+adb shell "pm uninstall -k --user 0 com.sec.android.app.clockpackage"
+# calendar
+adb shell "pm uninstall -k --user 0 com.samsung.android.calendar"
+# browser
+adb shell "pm uninstall -k --user 0 com.sec.android.app.sbrowser"
+# file viewer
+adb shell "pm uninstall -k --user 0 com.sec.android.app.myfiles"
+# gallery
+adb shell "pm uninstall -k --user 0 com.sec.android.gallery3d"
