602845b95c5e521ccab9b000bc3722eab8ba8821
Michael Stewart <mikrostew@gmail.com>
2021-02-08 16:03:29 -0800

    redo this to be much nicer

--- scripts/samsung-bloatware-remove.sh
+++ scripts/samsung-bloatware-remove.sh
@@ -13,67 +13,142 @@
 # $ adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.agent"
 # Failure [not installed for 0]
 
-
 # can find most of these using `adb shell` then `pm list packages -f <name>`
 
 
-##########################
-# useless or worse
-##########################
+# colors
 
-# Bixby - this is terrible, no one should ever use it for anything
-adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.agent"
-adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.es.globalaction"
-adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.wakeup"
-adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.plmsync"
-adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.voiceinput"
-adb shell "pm uninstall -k --user 0 com.samsung.systemui.bixby"
-adb shell "pm uninstall -k --user 0 com.samsung.android.bixby.agent.dummy"
-adb shell "pm uninstall -k --user 0 com.samsung.android.bixbyvision.framework"
-adb shell "pm uninstall -k --user 0 com.samsung.android.app.spage"
+COLOR_RESET='\033[0m'
+COLOR_FG_BOLD_GREEN='\033[1;32m'
+COLOR_FG_GREEN='\033[0;32m'
+COLOR_FG_RED='\033[0;31m'
 
-# Yahoo - why?
-adb shell "pm uninstall -k --user 0 com.samsung.android.widgetapp.yahooedge.finance"
-adb shell "pm uninstall -k --user 0 com.samsung.android.widgetapp.yahooedge.sport"
+# helper functions
 
-# Flipboard
-adb shell "pm uninstall -k --user 0 flipboard.boxer.app"
+pkg-remove-info() {
+  action="$1"
+  details="$2"
 
-# Skype
-adb shell "pm uninstall -k --user 0 com.skype.raider"
+  echo -e -n " ${COLOR_FG_BOLD_GREEN}$action${COLOR_RESET} $details..."
+}
+pkg-found() {
+  echo -e -n "(${COLOR_FG_GREEN}found${COLOR_RESET})..."
+}
+pkg-not-found() {
+  echo -e "(not found)"
+}
+pkg-removed() {
+  echo -e "[${COLOR_FG_GREEN}OK${COLOR_RESET}]"
+}
+pkg-not-removed() {
+  echo -e "[${COLOR_FG_RED}ERROR${COLOR_RESET}]"
+}
+remove-pkg() {
+  pkg_name="$1"
 
-# Kids Installer - what is this?
-adb shell "pm uninstall -k --user 0 com.samsung.android.kidsinstaller"
+  pkg-remove-info "remove" "'$pkg_name'"
+  if [ -n "$(adb shell "pm list packages -f $pkg_name")" ]
+  then
+    pkg-found
+    # try to remove it
+    adb shell "pm uninstall -k --user 0 $pkg_name" >/dev/null && pkg-removed || pkg-not-removed
+  else
+    pkg-not-found
+  fi
+}
 
-# Samsung Cloud
-adb shell "pm uninstall -k --user 0 com.samsung.android.scloud"
-adb shell "pm uninstall -k --user 0 com.samsung.android.samsungpass"
-adb shell "pm uninstall -k --user 0 com.samsung.android.samsungpassautofill"
+# USB debugging check
 
-# Samsung Dex for PC
-adb shell "pm uninstall -k --user 0 com.sec.android.app.dexonpc"
+echo -n "USB debugging enabled? "
+if adb devices 2>/dev/null | grep 'device$' >/dev/null
+then
+  echo -e "[${COLOR_FG_GREEN}OK${COLOR_RESET}]"
+else
+  echo -e "[${COLOR_FG_RED}not enabled${COLOR_RESET}]"
+  echo "output of 'adb devices':"
+  adb devices
+  exit
+fi
 
-# Samsung Social
-adb shell "pm uninstall -k --user 0 com.samsung.android.app.social"
+# list of everything to remove
 
+bloatware=(
+  # Bixby - this is terrible, no one should ever use it for anything
+  com.samsung.android.bixby.agent
+  com.samsung.android.bixby.es.globalaction
+  com.samsung.android.bixby.wakeup
+  com.samsung.android.bixby.plmsync
+  com.samsung.android.bixby.voiceinput
+  com.samsung.systemui.bixby
+  com.samsung.android.bixby.agent.dummy
+  com.samsung.android.bixbyvision.framework
+  com.samsung.android.app.spage
+  com.samsung.android.app.settings.bixby
 
-##########################
-# duplicate apps
-##########################
+  # Yahoo - why?
+  com.samsung.android.widgetapp.yahooedge.finance
+  com.samsung.android.widgetapp.yahooedge.sport
 
-# messaging
-adb shell "pm uninstall -k --user 0 com.samsung.android.messaging"
-# email
-adb shell "pm uninstall -k --user 0 com.samsung.android.email.provider"
-# contacts
-adb shell "pm uninstall -k --user 0 com.samsung.android.contacts"
-# clock
-adb shell "pm uninstall -k --user 0 com.sec.android.app.clockpackage"
-# calendar
-adb shell "pm uninstall -k --user 0 com.samsung.android.calendar"
-# browser
-adb shell "pm uninstall -k --user 0 com.sec.android.app.sbrowser"
-# file viewer
-adb shell "pm uninstall -k --user 0 com.sec.android.app.myfiles"
-# gallery
-adb shell "pm uninstall -k --user 0 com.sec.android.gallery3d"
+  # Flipboard
+  flipboard.boxer.app
+
+  # Skype
+  com.skype.raider
+
+  # Kids Installer - what is this?
+  com.samsung.android.kidsinstaller
+
+  # Samsung Cloud
+  com.samsung.android.scloud
+  com.samsung.android.samsungpass
+  com.samsung.android.samsungpassautofill
+
+  # Samsung Dex for PC
+  com.sec.android.app.dexonpc
+
+  # Samsung Social
+  com.samsung.android.app.social
+
+  # TODO: not sure on these
+  #com.samsung.android.app.ledcoverdream
+  #com.samsung.android.smartswitchassistant
+  #com.samsung.android.app.galaxyfinder
+  #com.samsung.android.app.aodservice
+  #com.samsung.android.app.sbrowseredge
+  #com.samsung.android.game.gamehome
+  #com.samsung.android.app.tips
+  #com.samsung.android.app.talkback
+  #com.samsung.android.app.taskedge
+  #com.samsung.android.themecenter
+  #com.samsung.android.app.appsedge
+  #com.samsung.android.app.clipboardedge
+
+  # duplicate apps
+
+  # messaging
+  com.samsung.android.messaging
+  # email
+  com.samsung.android.email.provider
+  # contacts
+  com.samsung.android.contacts
+  com.samsung.android.app.contacts
+  # clock
+  com.sec.android.app.clockpackage
+  # calendar
+  com.samsung.android.calendar
+  com.samsung.android.opencalendar
+  # browser
+  com.sec.android.app.sbrowser
+  # file viewer
+  com.sec.android.app.myfiles
+  # gallery
+  com.sec.android.gallery3d.panorama360view
+  com.sec.android.gallery3d
+)
+
+# do it
+
+for package in "${bloatware[@]}"
+do
+  remove-pkg "$package"
+done
