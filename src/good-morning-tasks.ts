b0ba59a70b076c564f452b8a5c367e270b85449a
Michael Stewart <mikrostew@gmail.com>
2022-08-31 11:13:17 -0400

    fix script linting

--- src/good-morning-tasks.ts
+++ src/good-morning-tasks.ts
@@ -210,10 +210,16 @@ const config: Config = {
           syncConflictCheck(dir)
         )
       ),
-      exec('Lint script files', workMachines, 'lint-scripts', [`${__dirname}/*`], {
-        shell: true,
-        cwd: path.resolve(__dirname, '..'),
-      }),
+      exec(
+        'Lint script files',
+        workMachines,
+        'lint-scripts',
+        [`${__dirname}/../bin/*`, `${__dirname}/../src/*`],
+        {
+          shell: true,
+          cwd: path.resolve(__dirname, '..'),
+        }
+      ),
       exec('Cleanup shivs', workMachines, 'cleanup-shivs', [process.env['LDAP_PASS']!]),
       func('Disk space check', laptopMachines, async () => {
         const { stdout } = await execa('df', ['-h']);
