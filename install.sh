8815151177e0226187a849fa42b3f36c665f3b3e
Michael Stewart <mikrostew@gmail.com>
2022-10-19 19:46:03 -0400

    simplify script link + alias

--- install.sh
+++ install.sh
@@ -73,17 +73,11 @@ _wait-for-command() {
 # TODO: input options
 #  - update (the install dir)
 #  - prefix dir
-prompt_links=""
 prefix_dir=/usr/local
 
 while [ "$#" -gt 0 ]
 do
   case "$1" in
-    # prompt for each conflicting link, giving the option to overwrite
-    --prompt)
-      shift
-      prompt_links="true"
-      ;;
     *)
       echo "Unknown argument '$1'"
       exit 1
@@ -112,20 +106,19 @@ fi
 
 linked=0
 aliased=0
-skipped=0
-overwrote=0
-total=0
+current=0
 
-# TODO: use _wait-for-command with this mess
-# TODO: remove symlinks that are no longer used
-for repo_path in "$installed_bin_dir"/*
+links_to_process=( "$installed_bin_dir"/* )
+total_links="${#links_to_process[@]}"
+for repo_path in "${links_to_process[@]}"
 do
-  (( total++ ))
+  (( current++ ))
   # delete up to the last slash
   script="${repo_path##*/}"
   bin_path="$bin_dir/$script"
-  echo -n "$bin_path => $repo_path"
-  total_length=$(( ${#bin_path} + 4 + ${#repo_path} ))
+
+  echo -e -n "\r ($current/$total_links) $script "
+  total_length=$(( 2 + 3 + 1 + 3 + 2 + ${#script} + 1 ))
 
   # if the link exists, make sure it points to the right location
   if [ -L "$bin_path" ]
@@ -133,53 +126,16 @@ do
     link_target="$(readlink "$bin_path")"
     if [ "$link_target" != "$repo_path" ]
     then
-      if [ "$prompt_links" = "true" ]
+      # the link doesn't point to this repo,
+      # add an alias instead of failing or overwriting
+      if ! grep -F "alias $script=$repo_path" "$HOME/.bashrc" >/dev/null
       then
-        echo -e -n "\r${COLOR_FG_BOLD_BLUE}'$bin_path' links to '$link_target' - (A)lias/(O)verwrite/(S)kip? [A]${COLOR_RESET} "
-        read -r link_action
-        if [ "$link_action" == "A" ] || [ "$link_action" == "a" ] || [ -z "$link_action" ]
-        then
-          # alias (default)
-          if ! grep -F "alias $script=$repo_path" "$HOME/.bashrc" >/dev/null
-          then
-            echo "alias $script=$repo_path" >> "$HOME/.bashrc"
-            echo -e "(${COLOR_FG_YELLOW}aliased${COLOR_RESET})"
-            (( aliased++ ))
-          else
-            (( skipped++ ))
-          fi
-        elif [ "$link_action" == "O" ] || [ "$link_action" == "o" ]
-        then
-          # overwrite
-          rm "$bin_path"
-          ln -s "$repo_path" "$bin_path"
-          echo -e "(${COLOR_FG_RED}overwrite${COLOR_RESET})"
-          (( overwrote++ ))
-        elif [ "$link_action" == "S" ] || [ "$link_action" == "s" ]
-        then
-          # skip
-          echo -e "(${COLOR_FG_YELLOW}skipped${COLOR_RESET})"
-          (( skipped++ ))
-        else
-          exit
-        fi
-      else
-        # add an alias instead of failing or overwriting
-        if ! grep -F "alias $script=$repo_path" "$HOME/.bashrc" >/dev/null
-        then
-          echo "alias $script=$repo_path" >> "$HOME/.bashrc"
-          echo -e " (${COLOR_FG_YELLOW}aliased${COLOR_RESET}) in .bashrc"
-          (( aliased++ ))
-        else
-          printf "\r%-${total_length}s\r" ' '
-          (( skipped++ ))
-        fi
+        echo "alias $script=$repo_path" >> "$HOME/.bashrc"
       fi
+      (( aliased++ ))
     else
-      echo -e -n " (${COLOR_FG_YELLOW}skip${COLOR_RESET})"
-      (( total_length += 7 ))
-      printf "\r%-${total_length}s\r" ' '
-      (( skipped++ ))
+      # already points to this repo, nothing to do
+      (( linked++ ))
     fi
   # if a file exists there, don't mess with that
   elif [ -f "$bin_path" ]
@@ -188,29 +144,16 @@ do
     if ! grep -F "alias $script=$repo_path" "$HOME/.bashrc" >/dev/null
     then
       echo "alias $script=$repo_path" >> "$HOME/.bashrc"
-      echo -e " (${COLOR_FG_YELLOW}aliased${COLOR_RESET}) in .bashrc"
-      (( aliased++ ))
-    else
-      printf "\r%-${total_length}s\r" ' '
-      (( skipped++ ))
     fi
+    (( aliased++ ))
   else
     ln -s "$repo_path" "$bin_path"
-    echo -e -n " (${COLOR_FG_GREEN}OK${COLOR_RESET})"
-    (( total_length += 5 ))
-    printf "\r%-${total_length}s\r" ' '
     (( linked++ ))
   fi
+  printf "\r%-${total_length}s\r" ' '
 done
+printf "\r ${COLOR_FG_BOLD_GREEN}✔${COLOR_RESET} setup $total_links scripts: symlinked $linked, aliased $aliased\n"
 
-echo "$total scripts"
-echo "  linked:    $linked"
-echo "  aliased:   $aliased"
-echo "  skipped:   $skipped"
-
-if [ "$prompt_links" = "true" ]
-then
-  echo "  overwrote: $overwrote"
-fi
+# TODO: remove symlinks that are no longer used
 
 cd "$install_dir" && _wait-for-command yarn install
